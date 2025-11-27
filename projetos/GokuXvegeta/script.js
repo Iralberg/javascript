f32c45dd5d3238ab557ab82e70fc13cd33a66109
iralberg <iralbergsampaio@gmail.com>
2025-11-27 09:51:39 -0300

    background

--- projetos/GokuXvegeta/script.js
+++ projetos/GokuXvegeta/script.js
@@ -4,10 +4,10 @@ let foto=document.getElementById('foto')
 function salvar() {
     if (rad[0].checked) {
         res.innerHTML = '<h2>Goku</h2>'
-        foto.src="transferir.jpeg"
+        foto.src="goku.jpg"
     }
     else if (rad[1].checked) {
         res.innerHTML = `<h2>Vegeta</h2>`
-        foto.src='images.jpeg'
+        foto.src="vegeta.png"
     }
 }
